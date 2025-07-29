427b6d2c5335735f58865f3f2c2bc7e37e1ed088
Wemerson <nerdhits001@gmail.com>
2025-07-29 00:17:34 -0300

    feat(model): Adds input models for delivery, items, touchpoints, and delivery person ID

---
+++ Microservices/Delivery-Tracking/src/main/java/com/example/delivery/tracking/application/model/CourierIdInput.java
@@ -0,0 +1,13 @@
+package com.example.delivery.tracking.application.model;
+
+import java.util.UUID;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CourierIdInput {
+
+    private UUID courierId;
+}
