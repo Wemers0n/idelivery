ce8c630736820a005b55b7c2e5a36500f8b35448
Wemerson <nerdhits001@gmail.com>
2025-07-29 00:22:35 -0300

    feat(controller): adds endpoints to list and search deliveries by ID

--- Microservices/Delivery-Tracking/src/main/java/com/example/delivery/tracking/application/controller/DeliveryController.java
+++ Microservices/Delivery-Tracking/src/main/java/com/example/delivery/tracking/application/controller/DeliveryController.java
@@ -2,7 +2,11 @@ package com.example.delivery.tracking.application.controller;
 
 import java.util.UUID;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.PagedModel;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -10,9 +14,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.example.delivery.tracking.application.model.DeliveryInput;
 import com.example.delivery.tracking.domain.model.Delivery;
+import com.example.delivery.tracking.domain.repository.DeliveryRepository;
 import com.example.delivery.tracking.domain.service.DeliveryPreparationService;
 
 import jakarta.validation.Valid;
@@ -24,6 +30,7 @@ import lombok.RequiredArgsConstructor;
 public class DeliveryController {
 
     private final DeliveryPreparationService deliveryPreparationService;
+    private final DeliveryRepository deliveryRepository;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -36,4 +43,14 @@ public class DeliveryController {
         return deliveryPreparationService.edit(deliveryId, input);
     }
 
+    @GetMapping
+    public PagedModel<Delivery> findAll(@PageableDefault Pageable pageable){
+        return new PagedModel<>(deliveryRepository.findAll(pageable));
+    }
+
+    @GetMapping("/{deliveryId}")
+    public Delivery findById(@PathVariable UUID deliveryId){
+        return deliveryRepository.findById(deliveryId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
 }
