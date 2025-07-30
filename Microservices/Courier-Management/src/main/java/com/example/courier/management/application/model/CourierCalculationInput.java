115cdb920e3be6cddc5e33c705546003d46dda4a
Wemerson <nerdhits001@gmail.com>
2025-07-30 09:58:21 -0300

    feat(model): create CourierPayoutResultModel to store the courier payout fee value

---
+++ Microservices/Courier-Management/src/main/java/com/example/courier/management/application/model/CourierCalculationInput.java
@@ -0,0 +1,11 @@
+package com.example.courier.management.application.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CourierCalculationInput {
+
+    private Double distanceInKm;
+}
---
+++ Microservices/Courier-Management/src/main/java/com/example/courier/management/application/model/CourierPayoutResultModel.java
@@ -0,0 +1,15 @@
+package com.example.courier.management.application.model;
+
+import java.math.BigDecimal;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class CourierPayoutResultModel {
+
+    private BigDecimal payoutFee;
+}
