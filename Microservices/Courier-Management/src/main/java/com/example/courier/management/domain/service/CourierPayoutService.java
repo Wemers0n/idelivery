e256d692eb2cba21bf7e02206d007460b0d312a0
Wemerson <nerdhits001@gmail.com>
2025-07-30 09:58:50 -0300

    feat(service): implement CourierPayoutService to calculate courier payout based on distance

---
+++ Microservices/Courier-Management/src/main/java/com/example/courier/management/domain/service/CourierPayoutService.java
@@ -0,0 +1,16 @@
+package com.example.courier.management.domain.service;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CourierPayoutService {
+
+    public BigDecimal calculate(Double distanceInKm){
+        return new BigDecimal("10")
+                .multiply(new BigDecimal(distanceInKm))
+                .setScale(2, RoundingMode.HALF_EVEN);
+    }
+}
