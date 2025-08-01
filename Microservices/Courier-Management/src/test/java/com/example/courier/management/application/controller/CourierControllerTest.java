eb079078efe3a802c74b9ac0a4450364763aa6fe
Wemerson <nerdhits001@gmail.com>
2025-08-01 20:57:49 -0300

    test(controller): adds test for creating courier with status 201

---
+++ Microservices/Courier-Management/src/test/java/com/example/courier/management/application/controller/CourierControllerTest.java
@@ -0,0 +1,56 @@
+package com.example.courier.management.application.controller;
+
+import java.util.UUID;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+
+import com.example.courier.management.domain.model.Courier;
+import com.example.courier.management.domain.repository.CourierRepository;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CourierControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private CourierRepository courierRepository;
+
+    @BeforeEach
+    void setup(){
+        RestAssured.port = port;
+        RestAssured.basePath = "/api/v1/couriers";
+    }
+
+    @Test
+    public void shouldReturn201(){
+        String resquestBody = """
+                {
+                    "name" : "João da Silva",
+                    "phone" : "1195500050"
+                }
+                """;
+
+        RestAssured
+            .given()
+                .body(resquestBody)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+            .when()
+                .post()
+            .then()
+                .statusCode(HttpStatus.CREATED.value())
+                .body("id", Matchers.notNullValue())
+                .body("name", Matchers.equalTo("João da Silva"));
+    }
+
+}
