cef9f9d9a386bea0b269c7b2e71d74c70cef9f9a
Wemerson <nerdhits001@gmail.com>
2025-08-09 23:12:53 -0300

    feat(event): handle domain events and publish to Kafka
    - Added event listeners for DeliveryPlacedEvent, DeliveryPickUpEvent, and DeliveryFulFilledEvent
    - Integrated event handling with Kafka publishing using IntegrationEventPublisher
    - Log event details before publishing to Kafka
    - Use Kafka topic

--- Microservices/Delivery-Tracking/src/main/java/com/example/delivery/tracking/infraestructure/event/DeliveryDomainEventHandler.java
+++ Microservices/Delivery-Tracking/src/main/java/com/example/delivery/tracking/infraestructure/event/DeliveryDomainEventHandler.java
@@ -6,26 +6,34 @@ import org.springframework.stereotype.Component;
 import com.example.delivery.tracking.domain.event.DeliveryFulFilledEvent;
 import com.example.delivery.tracking.domain.event.DeliveryPickUpEvent;
 import com.example.delivery.tracking.domain.event.DeliveryPlacedEvent;
+import com.example.delivery.tracking.infraestructure.kafka.IntegrationEventPublisher;
+import com.example.delivery.tracking.infraestructure.kafka.KafkaTopicConfig;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Component
 @Slf4j
-// @RequiredArgsConstructor
+@RequiredArgsConstructor
 public class DeliveryDomainEventHandler {
+
+    private final IntegrationEventPublisher integrationEventPublisher;
 
     @EventListener
     public void handlePlacedEvent(DeliveryPlacedEvent event){
         log.info(event.toString());
+        integrationEventPublisher.publish(event, event.getDeliveryId().toString(), KafkaTopicConfig.deliveryEventTopicName);
     }
 
     @EventListener
     public void handlePickUpEvent(DeliveryPickUpEvent event){
         log.info(event.toString());
+        integrationEventPublisher.publish(event, event.getDeliveryId().toString(), KafkaTopicConfig.deliveryEventTopicName);
     }
 
     @EventListener
     public void handleFulfilledEvent(DeliveryFulFilledEvent event){
         log.info(event.toString());
+        integrationEventPublisher.publish(event, event.getDeliveryId().toString(), KafkaTopicConfig.deliveryEventTopicName);
     }
 }
