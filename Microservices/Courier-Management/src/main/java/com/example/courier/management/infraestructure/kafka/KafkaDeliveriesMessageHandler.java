c87f06d5852b17028814e6c285ae9559cd392af7
Wemerson <nerdhits001@gmail.com>
2025-08-12 20:55:46 -0300

    feat(kafka): Add handler for deliveries.v1.events topic events
    - Implemented `KafkaDeliveriesMessageHandler` to process events from the `deliveries.v1.events` Kafka topic. The handler supports the `DeliveryPlacedIntegrationEvent` and `DeliveryFulFilledIntegrationEvent` events, in addition to a default handler for unmapped types.
    
    - This addition allows processing different types of messages received in the Courier-Management context.

---
+++ Microservices/Courier-Management/src/main/java/com/example/courier/management/infraestructure/kafka/KafkaDeliveriesMessageHandler.java
@@ -0,0 +1,36 @@
+package com.example.courier.management.infraestructure.kafka;
+
+import org.springframework.kafka.annotation.KafkaHandler;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import com.example.courier.management.infraestructure.event.DeliveryFulFilledIntegrationEvent;
+import com.example.courier.management.infraestructure.event.DeliveryPlacedIntegrationEvent;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@KafkaListener(topics = {
+    "deliveries.v1.events"
+}, groupId = "courier-management")
+@Slf4j
+@RequiredArgsConstructor
+public class KafkaDeliveriesMessageHandler {
+
+    @KafkaHandler(isDefault = true)
+    public void defaultHandler(@Payload Object object){
+        log.info("Default Handler: {}", object);
+    }
+
+    @KafkaHandler
+    public void handler(@Payload DeliveryPlacedIntegrationEvent event){
+        log.info("Received {}", event);
+    }
+
+    @KafkaHandler
+    public void handler(@Payload DeliveryFulFilledIntegrationEvent event){
+        log.info("Received {}", event);
+    }
+}
