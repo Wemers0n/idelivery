73595752db728973d6ce3d4b8505b8313d5af4a6
Wemerson <nerdhits001@gmail.com>
2025-08-14 21:58:09 -0300

    starting the implementation of the idelivery gateway

---
+++ Microservices/iDelivery-Gateway/src/main/java/com/example/ideliverygateway/IDeliveryGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.ideliverygateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IDeliveryGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IDeliveryGatewayApplication.class, args);
+	}
+
+}
---
+++ Microservices/iDelivery-Gateway/src/test/java/com/example/ideliverygateway/IDeliveryGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ideliverygateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IDeliveryGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
