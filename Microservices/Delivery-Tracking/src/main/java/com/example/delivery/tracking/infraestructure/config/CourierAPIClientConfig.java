67cf22c778e9285905ffdb9bc8c75a03fc037f9c
Wemerson <nerdhits001@gmail.com>
2025-08-17 15:55:10 -0300

    feat(config): add connection and read timeouts to CourierAPIClient's RestClient
    Added a generateClientRequestFactory method to configure connection and read timeouts
    on the SimpleClientHttpRequestFactory used by RestClient. This improves resilience and latency control
    in requests to the courier-management service.

--- Microservices/Delivery-Tracking/src/main/java/com/example/delivery/tracking/infraestructure/config/CourierAPIClientConfig.java
+++ Microservices/Delivery-Tracking/src/main/java/com/example/delivery/tracking/infraestructure/config/CourierAPIClientConfig.java
@@ -1,8 +1,12 @@
 package com.example.delivery.tracking.infraestructure.config;
 
+import java.time.Duration;
+
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -20,13 +24,22 @@ public class CourierAPIClientConfig {
 
     @Bean
     public CourierAPIClient courierAPIClient(RestClient.Builder builder){
-        RestClient restClient = builder.baseUrl("http://courier-management").build();
+        RestClient restClient = builder.baseUrl("http://courier-management")
+            .requestFactory(generateClientRequestFactory())
+            .build();
 
         RestClientAdapter adapter = RestClientAdapter.create(restClient);
 
         HttpServiceProxyFactory proxyFactory = HttpServiceProxyFactory.builderFor(adapter).build();
 
         return proxyFactory.createClient(CourierAPIClient.class);
+    }
+
+    private ClientHttpRequestFactory generateClientRequestFactory(){
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(Duration.ofMillis(10));
+        factory.setReadTimeout(Duration.ofMillis(200));
+        return factory;
     }
 
 }
